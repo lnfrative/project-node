51d568e2174a2defa3a1751b0abd4413d1bc9065
Christopher Díaz <38027442+lnfrative@users.noreply.github.com>
2022-08-14 12:36:43 -0500

    update summoner interface
    prisma model

--- interfaces/Summoner/index.ts
+++ interfaces/Summoner/index.ts
@@ -1,12 +1,11 @@
 interface Summoner {
   id: string
   region: string
-  riotId: string
   summonerId: string
   accountId: string
   puuid: string
   name: string
   profileIconId: number
-  revisionDate: number
+  revisionDate: bigint
   summonerLevel: number
 }
